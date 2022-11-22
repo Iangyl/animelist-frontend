913a938a1a29686a0ee65c440072dd6b019d5a53
Volodymyr Barybin <vohanchib@gmail.com>
2022-11-22 22:56:27 +0200

    added file uploading hook

---
+++ src/hooks/useFileUpload.ts
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { apiPaths } from 'utils/constants';
+
+export default function useFileUpload() {
+  const [fileIds, setFileIds] = useState<string>('');
+
+  const uploadFile = async (data: File) => {
+    const formData = new FormData();
+    formData.append('img', data);
+    try {
+      const response = await fetch(apiPaths.uploadFile, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: formData,
+      });
+      const result = await response.json();
+
+      setFileIds(result);
+    } catch (e: unknown) {
+      console.error(e);
+    }
+  };
+
+  return {fileIds, uploadFile};
+}
