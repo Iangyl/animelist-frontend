b46ce31883b4063c18c34f46a172b93aa9b4b95d
Volodymyr Barybin <vohanchib@gmail.com>
2022-11-23 02:55:39 +0200

    fixed file upload

--- src/hooks/useFileUpload.ts
+++ src/hooks/useFileUpload.ts
@@ -7,12 +7,13 @@ export default function useFileUpload() {
   const uploadFile = async (data: File) => {
     const formData = new FormData();
     formData.append('img', data);
+
     try {
       const response = await fetch(apiPaths.uploadFile, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        /*  headers: {
+          'Content-Type': 'multipart/form-data',
+        }, */ // or this header or using FormData
         body: formData,
       });
       const result = await response.json();
